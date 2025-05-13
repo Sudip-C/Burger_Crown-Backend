5edacccfa04d04c26a98636538dda1f4f1130c78
sudip-interleap <sudip.chowdhury001@gmail.com>
2025-05-13 10:00:17 +0530

    .

--- src/middlewares/adminOnly.js
+++ src/middlewares/adminOnly.js
@@ -1,5 +1,5 @@
 const adminOnly = (req, res, next) => {
-    if (req.user.role !== 'Admin') {
+    if (req.user.role !== 'admin') {
       return res.status(403).json({ message: 'Admins only' });
     }
     next();
