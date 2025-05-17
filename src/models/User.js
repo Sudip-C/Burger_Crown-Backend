22331b297190a039aca40e3c0dc268c2c187a8c5
sudip-interleap <sudip.chowdhury001@gmail.com>
2025-05-17 10:43:55 +0530

    .

--- src/models/User.js
+++ src/models/User.js
@@ -24,4 +24,4 @@ userSchema.methods.matchPassword = function (enteredPassword) {
     return bcrypt.compare(enteredPassword, this.password);
   };
 
-module.exports = mongoose.model("User",userSchema)
+module.exports = mongoose.model("BUser",userSchema)
